f732131604e49e50623b7abe120334db177eeae5
João Pedro de Pieri <joaopbs20111@gmail.com>
2025-10-29 12:36:34 +0000

    Added basic FIFO

--- model/FIFO.java
+++ model/FIFO.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.List;
+
 public class FIFO implements Scheduler {
 
+    @Override
+    public Process nextProcess(List<Process> processes, int currentTime) {
+        // FIFO: pick the first READY process that arrived
+        for (Process p : processes) {
+            if (!p.isCompleted() && p.getStartTime() <= currentTime) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "FIFO";
+    }
 }
