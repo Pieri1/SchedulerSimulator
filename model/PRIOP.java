6a16648cbd5ee2c7bf55fea6974a2b013fe91f5b
João Pedro de Pieri <joaopbs20111@gmail.com>
2025-10-29 12:36:26 +0000

    added generic PRIOP and SRTF

--- model/PRIOP.java
+++ model/PRIOP.java
@@ -1,5 +1,20 @@
 package model;
 
+import java.util.List;
+
 public class PRIOP implements Scheduler {
 
+	@Override
+	public Process nextProcess(List<Process> processes, int currentTime) {
+		if (processes == null || processes.isEmpty()) {
+			return null;
+		}
+		// simple default: return the first available process
+		return processes.get(0);
+	}
+
+	@Override
+	public String getName() {
+		return "PRIOP";
+	}
 }
--- model/SRTF.java
+++ model/SRTF.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.util.List;
+
 public class SRTF implements Scheduler {
 
+
+	@Override
+	public Process nextProcess(List<Process> processes, int currentTime) {
+		if (processes == null || processes.isEmpty()) {
+			return null;
+		}
+		// simple default: return the first available process
+		return processes.get(0);
+	}
+
+	@Override
+	public String getName() {
+		return "SRTF";
+	}
 }
